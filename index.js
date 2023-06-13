bee528682be4f1427714ced653eca4bd573b5e7c
vintagediary <dsyadavnda@gmail.com>
2023-06-13 18:43:16 +0530

    js learn1

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ console.log(a);
 a="hello";
 console.log(a);
 console.log(typeof a);
+a=true;
+console.log(typeof a);
+var num=10;
+console.log(num);
+//boolean
+var t =true;
+var f=false;
+console.log(t);
+console.log(f);
+//string
+// ` `," ",' ' backtick,double qutes, sigle quates
+var str= 'a';
+console.log(typeof str);
+str = "how are you" ;
+console.log(str);
+//backslash n("\n") to use next line
+var b;
+b = "how are you \n i am fine how about you" ;
+console.log(b);
+
+var num=300;
+console.log(`half of ${num} is ${num/2}`)
+
