b1609969e8328b6414727700eb6674ed1faa7e85
vintagediary <dsyadavnda@gmail.com>
2023-06-22 16:09:25 +0530

    function

---
+++ function.js
@@ -0,0 +1,41 @@
+// 3 types of function
+//Normal function
+//function defination
+//function function_name(param1, para2) {
+//do something
+//}
+//fuction invoke
+//function_name(arg1,arg2)
+//function add (a,b){
+ //   console.log(a+b)
+//}
+//add(2,5)
+
+//fuctions are treated as first class citezens in javascript
+//-> fuction can be rturned 
+//->functions can be passed as parameters/argument 
+
+//function calculator(str,a,b){
+//  if(str=='add'){
+//  return function add(){  //      console.log(a+b);  //}
+//}
+//}
+//let returnedFuc=calculator('add',2,3);
+//console.log("returned fuction is\n" +returnedFuc);
+//returnedFuc();
+
+
+// Function expression
+//let variable_name=function(){
+//do somthing
+//}
+// name of variable is used to invoke the fuction
+//variable_name();
+
+//let sayhi =function abcd(){
+//    console.log("hello this is function epression")
+//}
+//sayhi();
+//abcd(); error abcd is notdefined
+
+
